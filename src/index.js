3814197e0757fa929bc5018f96eb6d462d1395b4
Hind Rami ♕ <ramihind@MacBook-Pro-de-Hind.local>
2022-05-09 19:48:41 +0200

    Version Alpha prete a etre codé

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+import App from './App';
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
+
+const index = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default index;
